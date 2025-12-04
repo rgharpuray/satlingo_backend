762528004a17a4cbffe5805ab8315c27a604bc96
Rishi <rishi@Rishis-MacBook-Pro-2.local>
2025-12-03 21:35:31 -0800

    procfile

--- satlingo/settings.py
+++ satlingo/settings.py
@@ -4,6 +4,7 @@ Django settings for satlingo project.
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
